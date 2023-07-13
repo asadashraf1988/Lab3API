a6a9e45981e4cc3ec891095856b956cb58dde2f9
asadashraf1988 <133050338+asadashraf1988@users.noreply.github.com>
2023-07-12 20:37:26 -0400

    Add project files.

---
+++ Controllers/Lab3Controller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Lab3Controller : ControllerBase
+    {
+        private static List<Lab3> users = new List<Lab3>
+            {
+                new Lab3 { studentId = 8864578, name = "Asad" },
+                new Lab3 { studentId = 8864000, name = "Dummy" }
+            };
+
+        // 1. Get All Users (Get)
+        [HttpGet]
+        public async Task<ActionResult<List<Lab3>>> Get()
+        {
+            return Ok(users);
+        }
+
+        // 2. Get a user detail by providing user ID (Get)
+        [HttpGet("userDetailById")]
+        public async Task<ActionResult<Lab3>> Get(int userDetailById)
+        {
+            var user = users.Find(i => i.studentId == userDetailById);
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+            else
+            {
+                return Ok(user);
+            }
+        }
+
+        // 3. Add a new user (Post)
+        [HttpPost]
+        public async Task<ActionResult<List<Lab3>>> AddUser(Lab3 user)
+        {
+            users.Add(user);
+            return Ok(user);
+        }
+
+        // 4. Update existing user (Put) 
+
+
+    }
+}
---
+++ Lab3.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab3API
+{
+    public class Lab3
+    {
+        public int studentId { get; set; }
+        public string name { get; set; } = string.Empty;
+    }
+}
